48e8d70b141ce644f2614d3c78784fe84b03f658
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-10-12 23:02:33 +0300

    created book model

---
+++ src/models/book-model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const BookSchema = mongoose.Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    ISBN: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    author: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Author',
+        require: true
+    },
+    publisher: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Publisher',
+        require: true
+    },
+    comments: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'Comment',
+    }]
+})
+
+const BookModel = mongoose.Model('Book', BookSchema)
+
+module.exports = BookModel
