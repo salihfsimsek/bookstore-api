9c46b8cf6c905e15ba04bee8c9a7551a9f181675
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-10-14 22:30:04 +0300

    created base service

---
+++ src/services/base-service.js
@@ -0,0 +1,28 @@
+class BaseModel {
+    //Find all
+    async findAll() {
+        return this.model.find()
+    }
+
+    //Find spesific one
+    async find(object) {
+        return this.model.findOne(object)
+    }
+
+    //create
+    async create(item) {
+        return this.model.create(item)
+    }
+
+    //update
+    async update(id, object) {
+        return this.model.findOneAndUpdate(id, object, { new: true })
+    }
+
+    //delete
+    async delete(id) {
+        return this.model.deleteOne({ _id: id })
+    }
+}
+
+module.exports = BaseModel
