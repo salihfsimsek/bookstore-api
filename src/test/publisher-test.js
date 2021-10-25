641eca97f4398e4b3f3d70acc7870d93061a504f
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-10-25 23:02:58 +0300

    worked on tests

---
+++ src/test/publisher-test.js
@@ -0,0 +1,17 @@
+//////Test packages//////
+const test = require('ava')
+const request = require('supertest')
+
+const app = require('../../app')
+
+const PublisherModel = require('../models/publisher-model')
+
+test.todo('Create publisher')
+
+test.todo('Update publisher')
+
+test.todo('get publisher')
+
+test.todo('get publishers')
+
+test.todo('delete publisher')
