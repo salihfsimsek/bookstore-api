6cb64e6ec20b96ac6532029d4521cc2d42aa46f4
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-11-05 23:12:49 +0300

    file helper fixed

--- src/scripts/utils/fileHelper.js
+++ src/scripts/utils/fileHelper.js
@@ -1,4 +1,17 @@
 const multer = require('multer')
-const upload = multer({ dest: __dirname + '/upload/images' })
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
 
 module.exports = { upload }
